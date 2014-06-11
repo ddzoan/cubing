03b6dcf168b7c54efd762b2e0b090c84866df518
Dan Dzoan <ddzoan@helixeducation.com>
2014-06-11 11:44:13 -0700

    mend

--- scrambler.rb
+++ scrambler.rb
@@ -2,34 +2,40 @@ def getScramble()
   allturns = ["L","L2","L'","R","R2","R'","U","U2","U'","D","D2","D'","F","F2","F'","B","B2","B'"]
   
   scramble = []
+  prevFace = 0
+  prevAxis = 0
+  prevPrevAxis = 0
+
   25.times do |i|
     valid = false
 
     while !valid
-      randindex = (18*rand).floor
+      randTurnID = (18*rand).floor
 
       if i > 0
-        if (randindex / 3) == (prevTurn) / 3)
+        if (randTurnID / 3) == prevFace
           valid = false
-        elsif i > 1 && (randindex / 6) == (prevTurn) / 6) && (randindex / 3) == (prevPrevTurn) / 3)
+        elsif i > 1 && (randTurnID / 6) == prevPrevAxis && (randTurnID / 6) == prevAxis
           valid = false
         else
           valid = true
-          prevPrevTurn = prevTurn if i > 1
-          prevTurn = randindex
+          prevPrevAxis = prevAxis if i > 1
+          prevFace = randTurnID / 3
+          prevAxis = randTurnID / 6
         end
       else
         valid = true
-        prevTurn = randindex
+        prevFace = randTurnID / 3
+        prevAxis = randTurnID / 6
       end
     end
 
     valid = false
 
-    scramble << allturns[randindex]
+    scramble << allturns[randTurnID]
   end
 
-  scramble.join
+  scramble.join(' ')
 end
 
 puts getScramble
