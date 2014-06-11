437c944603349e14a11a5822a0e8a763a210819d
Dan Dzoan <ddzoan@helixeducation.com>
2014-06-11 12:33:25 -0700

    initialize prev vars to -1

--- scrambler.rb
+++ scrambler.rb
@@ -2,9 +2,9 @@ def getScramble()
   allturns = ["L","L2","L'","R","R2","R'","U","U2","U'","D","D2","D'","F","F2","F'","B","B2","B'"]
   
   scramble = ""
-  prevFace = 0
-  prevAxis = 0
-  prevPrevAxis = 0
+  prevFace = -1
+  prevAxis = -1
+  prevPrevAxis = -1
 
   randTurnID = (allturns.length * rand).floor
   scramble << allturns[randTurnID] << " "
